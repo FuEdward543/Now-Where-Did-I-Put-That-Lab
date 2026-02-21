ec926094b739cfc792b5686d319b9c1170218e54
Edward Fu <73083011+FuEdward543@users.noreply.github.com>
2026-02-20 22:30:39 -0600

    Complete main/driver

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 #include "src/where.hpp"
 
 int main() {
-  // Your driver program goes here
+  char searchForChar;
+  string givenString;
+  cout << "Input String: ";
+  cin >> givenString;
+  cout << "Search for a Character? Y or N";
+  cin >> searchForChar;
+  if (searchForChar == 'Y'||searchForChar == 'y') {
+    char searchChar;
+    cout << "Enter a character: ";
+    cin >> searchChar;
+    cout << findWithinString(givenString, searchChar);
+  } else {
+    string searchString;
+    cout << "Enter a string: ";
+    cin >> searchString;
+    cout << findWithinString(givenString, searchString);
+  }
 }
